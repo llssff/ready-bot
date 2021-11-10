bdf6085e4a5748618dbf325e6eac8c9e3c06ee06
llssff <longlongsf@gmail.com>
2021-11-09 21:50:29 -0800

    dsjfasdf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import re
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -12,9 +13,15 @@ message_map = {}
 
 
 @bot.command(name='ready')
-async def create(ctx, count: int, time=None):
-    msg = await ctx.send(count)
-    message_map[msg] = set()
+async def create(ctx, count: int, content="chilling"):
+
+    #retard check
+    if count == 1:
+        await ctx.message.delete()
+        return
+
+    msg = await ctx.send(f'{count} attendees\nPurpose: {content}')
+    message_map[msg] = {ctx.message.author}
     await msg.add_reaction(fire_emoji)
     await ctx.message.delete()
 
@@ -49,15 +56,15 @@ async def on_reaction_add(reaction, user):
 
     # Add user to list
     message_map[msg].add(user)
-
-    param_count = int(msg.content)
+    param_count = int(re.search(r'\d+', msg.content).group(0))
     users = message_map[msg]
     if len(users) != param_count:
         return
 
-    channel = msg.channel
+    # channel = msg.channel
+    param_purpose = "\n".join(msg.content.split("\n")[1:])
     for user in message_map[msg]:
-        await user.send("Get In Here!")
+        await user.send(f"You've been summoned for:\n{param_purpose}")
 
     message_map.pop(msg)
     await msg.delete()
